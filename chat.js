db28927d9d51c889756cf7a3f85c14f82a5aa70f
Deepak Maurya <74600250+helodeepakji@users.noreply.github.com>
2021-09-07 14:51:01 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,20 +1,43 @@
+
+          
+
+
+            
+
 let reply = {
+
           
+
             "hii" : "hello kese h aap",
+
             
+
             "hello" : "  Hi there!",
-           "i love you" : "I Love You 2",
+
+      "i love you" : "I Love You 2",
+
             
+
             "who are you" : " I am Deepak's Assistant",
+
             
+
           "how are you" : "I am Fine",
+
             
+
             "how old are you" : " I am infinity in age",
+
             
-            "what is your name" : " My name is chatbot create by helodeepakji",
-          
-            "i m fine" : "good",
-             
-            "i am fine" : "great"
+
+      "what is your name" : " My name is chatbot create by helodeepakji",
+
+  
+
+  "i m fine" : "good",
+
+  "i am fine" : "great"
+
             
+
         };
