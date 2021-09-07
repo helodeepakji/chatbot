0411e237fd6f0ff4c43dce80a62d4946152109bd
Deepak Maurya <74600250+helodeepakji@users.noreply.github.com>
2021-09-07 14:23:00 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -12,5 +12,11 @@ let reply = {
             "how old are you?" : " I am infinity in age",
             
             "what is your name" : " My name is chatbot create by helodeepakji"
+     
+          "i love you" : "I Love You 2"
+
+          "i m fine"   : "good" 
+
+          "i am fine"  : "great"
             
         };
