ef7b8ac11fbf04aec71b1f900ec2c9a6fecb599e
Deepak Maurya <74600250+helodeepakji@users.noreply.github.com>
2021-09-07 12:59:07 +0530

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,16 @@
+let reply = {
+          
+            "hii" : "hello kese h aap",
+            
+            "hello" : "  Hi there!",
+
+
+            "who are you" : " I am Deepak's Assistant",
+            
+            "how are you" : "I am Fine",
+            
+            "how old are you?" : " I am infinity in age",
+            
+            "what is your name" : " My name is chatbot create by helodeepakji"
+            
+        };
